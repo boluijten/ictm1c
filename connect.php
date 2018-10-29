f0f0e4b76cf0b6847a558ae9df1cc527826c107a
Bo Luijten <bo@studiobasalt.com>
2018-10-29 15:20:14 +0100

    eerste comment

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "boluijten_com_database1";
+$password = "XkWZ4ZK63ykj";
+$dbname = "boluijten_com_database1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
