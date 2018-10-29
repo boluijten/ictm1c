39dcd86d3950a611d4656590796920ba914ef3a4
gerrlof <gerlof.komduur@hotmail.com>
2018-10-29 15:43:00 +0100

    Gerlof aangepast in titel

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Gerlof</title>
     <link rel="stylesheet" href="style.css">
     <?php include("check.php")?>
     <script type="javascript" rel="javascript.js"></script>
